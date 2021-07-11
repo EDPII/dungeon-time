53bc6d37a9c8165e32fc11b7ebd3a3725e2229f7
Eduardo Pires <dudupiresrosa@gmail.com>
2021-07-11 02:06:52 -0300

    feat: add door img to home page
    The Dungeon Time home page needed a new look

--- src/pages/api/auth/[...nextauth].tsx
+++ src/pages/api/auth/[...nextauth].tsx
@@ -2,7 +2,7 @@ import { NextApiRequest, NextApiResponse } from 'next'
 import NextAuth from 'next-auth'
 import Providers from 'next-auth/providers'
 
-export default (req: NextApiRequest, res: NextApiResponse): Promise<void> => 
+export default async (req: NextApiRequest, res: NextApiResponse): Promise<void> =>
 NextAuth(req, res, {
   // Configure one or more authentication providers
   providers: [
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -33,35 +33,37 @@ const Login: React.FC<AppProps> = ({ ...pageProps }) => {
             
 
             <section className={styles.sectionContainer}>
-            
+                <div>
+                    <img src="icons/wood-door.png" alt="wood-door"></img>
+                </div>
                 <div className={styles.container}>
-                <header>Bem-Vindo</header>
-                <main>
-                    <p>
-                        Faça login e começe a sua jornada.
-                    </p>
-                    <div className={styles.buttonsContainer}>
-                        <button className={styles.git} type="button" 
-                        onClick={() => signIn('github')}>
-                        <FaGithub /> Conectar com o GitHub
-                        </button>
-                        <button className={styles.gg} type="button" 
-                        onClick={() => signIn('google')}>
-                        <FaGoogle /> Conectar com o Google
-                        </button>
-                        <button className={styles.tt} type="button" 
-                        onClick={() => signIn('twitter')}>
-                        <FaTwitter /> Conectar com o Twitter
-                        </button>
-                    </div>    
-                </main>
-                <footer>
-                    <small>
-                        <strong>Dungeon Time</strong> é um timer baseado
-                        na técnica Pomodoro, com ele você foca mais e ganha um 
-                        auxílio na saúde e postura.
-                    </small>
-                </footer>
+                    <header>Bem-Vindo</header>
+                    <main>
+                        <p>
+                            Faça login e começe a sua jornada.
+                        </p>
+                        <div className={styles.buttonsContainer}>
+                            <button className={styles.git} type="button" 
+                            onClick={() => signIn('github')}>
+                            <FaGithub /> Conectar com o GitHub
+                            </button>
+                            <button className={styles.gg} type="button" 
+                            onClick={() => signIn('google')}>
+                            <FaGoogle /> Conectar com o Google
+                            </button>
+                            <button className={styles.tt} type="button" 
+                            onClick={() => signIn('twitter')}>
+                            <FaTwitter /> Conectar com o Twitter
+                            </button>
+                        </div>    
+                    </main>
+                    <footer>
+                        <small>
+                            <strong>Dungeon Time</strong> é um timer baseado
+                            na técnica Pomodoro, com ele você foca mais e ganha um 
+                            auxílio na saúde e postura.
+                        </small>
+                    </footer>
                 </div>
            </section>
           
